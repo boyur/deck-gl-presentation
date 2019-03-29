6242c092546cf8ba5921b9b547094314b91c1874
boyur <gboyur@gmail.com>
2019-03-29 19:05:25 +0300

    Updates

---
+++ precache-manifest.2c3297a507fc6ea9799a50dac422ec7a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "b6d6826aabccee5ac1ac",
+    "url": "/static/js/main.b6d6826a.chunk.js"
+  },
+  {
+    "revision": "bd868ab5aa38f38838c1",
+    "url": "/static/js/1.bd868ab5.chunk.js"
+  },
+  {
+    "revision": "bd868ab5aa38f38838c1",
+    "url": "/static/css/1.ad8e2a00.chunk.css"
+  },
+  {
+    "revision": "df863c7296cbc7be2c88ea8b49e57b2e",
+    "url": "/index.html"
+  }
+];
