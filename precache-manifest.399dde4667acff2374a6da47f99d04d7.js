5f9d877519b2c035cfe6ab68bbfeec3134d3dc6c
boyur <gboyur@gmail.com>
2019-04-07 20:09:48 +0300

    Updates

---
+++ precache-manifest.399dde4667acff2374a6da47f99d04d7.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "23d3b57636f52459aac18e52e4c1f6ce",
+    "url": "/deck-gl-presentation/static/media/Screenshot_1.23d3b576.png"
+  },
+  {
+    "revision": "ccc13c2c0f656c5dc895",
+    "url": "/deck-gl-presentation/static/js/main.ccc13c2c.chunk.js"
+  },
+  {
+    "revision": "1eea85398b4095590294fa6a9fdb20ba",
+    "url": "/deck-gl-presentation/static/media/qr.1eea8539.png"
+  },
+  {
+    "revision": "992bdc0ccd68d0d3b982",
+    "url": "/deck-gl-presentation/static/js/runtime~main.992bdc0c.js"
+  },
+  {
+    "revision": "1f6fe29f5f2c31d4f218fa1d7d517b28",
+    "url": "/deck-gl-presentation/static/media/after.1f6fe29f.png"
+  },
+  {
+    "revision": "70d501b5930645786d6aec3e170c666b",
+    "url": "/deck-gl-presentation/static/media/before.70d501b5.png"
+  },
+  {
+    "revision": "480034045bad1d9f61cba973c0e65851",
+    "url": "/deck-gl-presentation/static/media/Screenshot_2.48003404.png"
+  },
+  {
+    "revision": "ed2d3f729d64457402baa110c7033cad",
+    "url": "/deck-gl-presentation/static/media/composeLayer.ed2d3f72.png"
+  },
+  {
+    "revision": "2e1a87be54be3b63328d927fbc3a5363",
+    "url": "/deck-gl-presentation/static/media/ezgif.com-crop.2e1a87be.gif"
+  },
+  {
+    "revision": "3085e2474d4f7b32790dc0fccbdc7c6a",
+    "url": "/deck-gl-presentation/static/media/Screenshot_4.3085e247.png"
+  },
+  {
+    "revision": "fc638b9aa8bc0a67e11ec60d05c79dcb",
+    "url": "/deck-gl-presentation/static/media/Screenshot_3.fc638b9a.png"
+  },
+  {
+    "revision": "d646162a4fbf66d769f0",
+    "url": "/deck-gl-presentation/static/js/1.d646162a.chunk.js"
+  },
+  {
+    "revision": "d646162a4fbf66d769f0",
+    "url": "/deck-gl-presentation/static/css/1.c46035ca.chunk.css"
+  },
+  {
+    "revision": "0e8d14030375552f51943c7a72431dfa",
+    "url": "/deck-gl-presentation/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/deck-gl-presentation/precache-manifest.3e307345ebcd527501c8c789016047fd.js"
+  "/deck-gl-presentation/precache-manifest.399dde4667acff2374a6da47f99d04d7.js"
 );
 
 workbox.clientsClaim();
